a234123b85e080fdfaddf775e431e04470bc3b5f
Titlinath <titlinath3@gmail.com>
2025-11-09 23:12:10 +0530

    hemlo

--- StudySync/src/utils/colorUtils.js
+++ StudySync/src/utils/colorUtils.js
@@ -1,11 +1,11 @@
 export const getSubjectColor = (index) => {
   const colors = [
-    "from-[#8AC6D1] to-[#A3BFFA]",
-    "from-[#FF9A8B] to-[#FFD6A5]",
-    "from-[#DFF6F0] to-[#8AC6D1]",
-    "from-[#E8DFF5] to-[#A3BFFA]",
-    "from-[#A3BFFA] to-[#E9F0FF]",
-    "from-[#FFD6A5] to-[#FF9A8B]",
+    'from-[#8AC6D1] to-[#A3BFFA]',
+    'from-[#FF9A8B] to-[#FFD6A5]',
+    'from-[#DFF6F0] to-[#8AC6D1]',
+    'from-[#E8DFF5] to-[#A3BFFA]',
+    'from-[#A3BFFA] to-[#E9F0FF]',
+    'from-[#FFD6A5] to-[#FF9A8B]'
   ];
   return colors[index % colors.length];
 };
